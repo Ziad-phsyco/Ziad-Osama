65c70fdf77e34d6c0d316f7454cdcbf6e8ca5679
Ziad Osama <ziadosamaabdo@gmail.com>
2025-02-07 21:59:18 +0200

    rm

--- EmbeddedC/Unite3_L1/toggle_led/Src/main.c
+++ EmbeddedC/Unite3_L1/toggle_led/Src/main.c
@@ -1,3 +1,4 @@
+typedef volatile unsigned int vuint32_t;
 #include <stdint.h>
 
 #define RCC_BASE 	0x40021000
@@ -10,14 +11,19 @@
 #define GPIOA_P13 	(1UL<<13)
 
 
-typedef volatile unsigned int vuint32_t;
+
 typedef union {
-	vuint32_t 	all_fields;
-	struct {
-		vuint32_t reserved:13;
-		vuint32_t P13:1;
-	}Pin;
+		vuint32_t 	all_fields;
+		struct {
+		vuint32_t 	reserved:13;
+		vuint32_t 	P13:1;
+		}Pin;
 } R_ODR_t;
+
+unsigned char g_var[3] = {1,2,3};
+unsigned char const const_var[3] = {1,2,3};
+
+
 
 volatile R_ODR_t* R_ODR = (volatile R_ODR_t*)(GPIOA_BASE + 0x0C);
 
@@ -25,7 +31,8 @@ int main(void){
 	RCC_APB2ENR |= RCC_IOPAEN;
 	GPIOA_CRH 	&= 0xFF0FFFFF;
 	GPIOA_CRH 	|= 0x00200000;
-	while(1){
+	while(1)
+	{
 		R_ODR->Pin.P13 = 1;
 		for (int i =0; i<5000; i++);
 		R_ODR->Pin.P13 = 0;
